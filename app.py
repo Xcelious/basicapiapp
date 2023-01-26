1c6c066257546f85fe2811e6b29bede88b55e3da
Xcelious <dlawson3@cougarnet.uh.edu>
2023-01-25 23:53:58 -0600

    added unique id functionality

--- app.py
+++ app.py
@@ -1,56 +1,55 @@
+import uuid
 from flask import Flask, request
+from db import stores, items
+
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name": "My Store",
-        "items": [
-            {
-                "name": "tomato",
-                "price": 2.49
-            }
-        ]
-    }
-]
 
 @app.get("/store")      #http://127.0.0.1:5000/store
 def get_stores() -> dict: 
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
 
 # retrieves the entire store's contents
-@app.get("/store/<string:name>")    #http://127.0.0.1:5000/store/<name>
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
-    
-    return {"message": "Store not found"}, 404
+@app.get("/store/<string:store_id>")    #http://127.0.0.1:5000/store/<name>
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {"message": "Store not found"}, 404
 
 # retrieves items from a specific store
-@app.get("/store/<string:name>/item")   #http://127.0.0.1:5000/store/<name>/item
-def get_item_in_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"items": store["items"]}
-    return {"message": "Store not found"}, 404
+@app.get("item/<string:item_id>")   #http://127.0.0.1:5000/store/<name>/item
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "Item not found"}, 404
 
 @app.post("/store")     #http://127.0.0.1:5000/store
 def create_stores() -> dict:
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
-    
-    return new_store, 201
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex     #generates unique id per resource
 
-@app.post("/store/<string:name>/item")
+    # unpacks the values from the <store_data> dict and adds
+    # to the <store> dict, along with the unique ID
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+
+    return store, 201
+
+@app.post("/item")
 def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item, 201
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
 
-    return {"message": "Store not found"}, 404
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
 
+    return item, 201
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
